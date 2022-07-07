79f7b5fc23d79431b17f17d8979a732a9ba0bf50
Harsha Vardhan <munharsha@gmail.com>
2022-07-07 15:31:23 +0800

    Make Login page

--- src/components/MainButton.tsx
+++ src/components/MainButton.tsx
@@ -1,3 +1,6 @@
+import { View, Text } from "react-native";
+import React from "react";
+
 import { Button } from "react-native-paper";
 
 const MainButton = ({ onPress, children }) => {
--- src/screens/LoginScreen.tsx
+++ src/screens/LoginScreen.tsx
@@ -1,14 +1,55 @@
-import { View, Text } from "react-native";
+import { View, Text, StyleSheet, Image } from "react-native";
+import React from "react";
 
 import MainButton from "components/MainButton";
+import { TextInput } from "react-native-paper";
 
 const LoginScreen = ({ navigation }) => {
+  const [usernameText, setUsernameText] = React.useState("");
+  const [passwordText, setPasswordText] = React.useState("");
+
   return (
-    <View>
-      <Text>Login Screen</Text>
-      <MainButton onPress={() => navigation.navigate("GSWhere")}>
-        Login
-      </MainButton>
+    <View
+      style={[
+        {
+          flexDirection: "column",
+          justifyContent: "space-evenly",
+          alignItems: "center",
+          flex: 1,
+        },
+      ]}
+    >
+      <View>
+        <Image
+          source={require("../../assets/Goldman_Sachs.png")}
+          style={[
+            {
+              width: 300,
+              height: 300,
+              // justifyContent: "center",
+              // marginLeft: 140,
+            },
+          ]}
+        />
+      </View>
+
+      <View style={[{ width: "100%", marginTop: 10 }]}>
+        <TextInput
+          label="Username"
+          value={usernameText}
+          onChangeText={(text) => setUsernameText(text)}
+        />
+        <TextInput
+          label="Passsword"
+          value={passwordText}
+          onChangeText={(text) => setPasswordText(text)}
+        />
+      </View>
+      <View style={{}}>
+        <MainButton onPress={() => navigation.navigate("GSWhere")}>
+          Login
+        </MainButton>
+      </View>
     </View>
   );
 };
